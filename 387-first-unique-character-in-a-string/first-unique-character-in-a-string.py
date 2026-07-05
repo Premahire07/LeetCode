1fce7c4b70191d5be35c247ed4ceae63c2b44f8b
Prem Ahire <161350215+Premahire07@users.noreply.github.com>
2026-07-05 23:57:27 +0530

    Time: 77 ms (36.86%) | Memory: 19.6 MB (57.62%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
@@ -1,7 +1,13 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
+        freq = {}
+
+        for ch in s:
+            freq[ch] = freq.get(ch, 0) + 1
+
         for i in range(len(s)):
-            if s.count(s[i]) == 1:
+            if freq[s[i]] == 1:
                 return i
+
         return -1
         
