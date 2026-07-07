91f1f599931774bbb9fa3fd1c40a5f8efe080da5
Prem Ahire <161350215+Premahire07@users.noreply.github.com>
2026-07-07 23:50:54 +0530

    Time: 4 ms (43.34%) | Memory: 19.3 MB (23.29%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -1,5 +1,20 @@
-import math
-
 class Solution:
     def mySqrt(self, x: int) -> int:
-        return int(math.sqrt(x))
+        left = 0
+        right = x
+        ans = 0
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if mid * mid == x:
+                return mid
+
+            elif mid * mid < x:
+                ans = mid
+                left = mid + 1
+
+            else:
+                right = mid - 1
+
+        return ans
