bec5bc3bca8651d866c3172e62a0e259d463cb3e
Prem Ahire <161350215+Premahire07@users.noreply.github.com>
2026-08-17 00:51:17 +0530

    Time: 11 ms (31.33%) | Memory: 19.7 MB (52.62%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,10 +1,26 @@
 class Solution:
+    def isAlphanumeric(self,s):
+        x = ord(s)
+        if 97 <= x <= 122 or 65 <= x <= 90 or 48 <= x <= 57:
+            return True
+        return False
     def isPalindrome(self, s: str) -> bool:
-        text = ""
 
-        for ch in s:
-            if ch.isalnum():  # Checks if char is letter or number othe than that get ignored
-                text += ch.lower()
+        s = s.lower()
 
-        return text == text[::-1] 
+        i = 0
+        j = len(s) - 1
+
+        while i<j:
+            if not self.isAlphanumeric(s[i]):
+                i += 1
+            elif not self.isAlphanumeric(s[j]):
+                j -= 1
+            elif s[i] == s[j]:
+                i += 1
+                j -= 1
+            else:
+                return False
+        return True
+
         
